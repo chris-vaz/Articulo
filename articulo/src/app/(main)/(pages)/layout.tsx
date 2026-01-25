65abe2c47431ca36fd3b45f1e772bab4dae0b15a
chris-vaz <chrisvas2001@gmail.com>
2026-01-25 11:27:42 +0300

    fix: layout content overflow issue

--- articulo/src/app/(main)/(pages)/layout.tsx
+++ articulo/src/app/(main)/(pages)/layout.tsx
@@ -1,15 +1,15 @@
-import React from 'react'
+import React from "react"
 
 type Props = { children: React.ReactNode }
 
-const Layout = (props: Props) => {
-    return (
-        <div className="flex overflow-hidden h-screen">
-            <div className="w-full">
-                {props.children}
-            </div>
-        </div>
-    )
+const Layout = ({ children }: Props) => {
+  return (
+    <div className="flex h-screen">
+      <div className="w-full h-full overflow-y-auto">
+        {children}
+      </div>
+    </div>
+  )
 }
 
 export default Layout
