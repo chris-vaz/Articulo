1762533b6be4d95aece3849a7719ada21f4f2f8d
chris-vaz <chrisvas2001@gmail.com>
2026-01-27 13:05:44 +0300

    feat: user and workflow connection enhancements

--- articulo/src/components/forms/profile-form.tsx
+++ articulo/src/components/forms/profile-form.tsx
@@ -1,77 +1,106 @@
 'use client'
 
-import React, { useState } from 'react'
+import React, { use, useEffect, useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
+import { EditUserProfileSchema } from '@/lib/types'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '../ui/form'
 import { Input } from '../ui/input'
 import { Button } from '../ui/button'
 import { Loader2 } from 'lucide-react'
 
-const ProfileForm = () => {
+type Props = {
+  user: any
+  onUpdate?: any
+}
+
+const ProfileForm = ({ user, onUpdate }: Props) => {
   const [isLoading, setIsLoading] = useState(false)
-  const [name, setName] = useState('')
-  const [email, setEmail] = useState('')
+  const form = useForm<z.infer<typeof EditUserProfileSchema>>({
+    mode: 'onChange',
+    resolver: zodResolver(EditUserProfileSchema),
+    defaultValues: {
+      name: user.name,
+      email: user.email,
+    },
+  })
 
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault()
+  const handleSubmit = async (
+    values: z.infer<typeof EditUserProfileSchema>
+  ) => {
     setIsLoading(true)
-
-    // fake delay just for UI testing
-    setTimeout(() => {
-      setIsLoading(false)
-      console.log({ name, email })
-    }, 1000)
+    await onUpdate(values.name)
+    setIsLoading(false)
   }
 
+  useEffect(() => {
+    form.reset({ name: user.name, email: user.email })
+  }, [user])
+
   return (
-    <form
-      onSubmit={handleSubmit}
-      className="flex max-w-md flex-col gap-6"
-    >
-      {/* Name */}
-      <div className="flex flex-col gap-2">
-        <label className="text-lg font-medium">User full name</label>
-        <Input
-          placeholder="Name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          className="focus-visible:ring-2 focus-visible:ring-[#2F006B]"
-        />
-      </div>
-
-      {/* Email */}
-      <div className="flex flex-col gap-2">
-        <label className="text-lg font-medium">Email</label>
-        <Input
-          placeholder="Email"
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          className="focus-visible:ring-2 focus-visible:ring-[#2F006B]"
-        />
-      </div>
-
-      {/* Submit */}
-      <Button
-        type="submit"
-        disabled={isLoading}
-        className="
-          self-start
-          bg-white text-black
-          hover:bg-[#2F006B] hover:text-white
-          transition-colors
-          disabled:cursor-not-allowed
-          disabled:opacity-70
-        "
+    <Form {...form}>
+      <form
+        className="flex flex-col gap-6"
+        onSubmit={form.handleSubmit(handleSubmit)}
       >
-        {isLoading ? (
-          <>
-            <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-            Saving
-          </>
-        ) : (
-          'Save User Settings'
-        )}
-      </Button>
-    </form>
+        <FormField
+          disabled={isLoading}
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="text-lg">User full name</FormLabel>
+              <FormControl>
+                <Input
+                  {...field}
+                  placeholder="Name"
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="text-lg">Email</FormLabel>
+              <FormControl>
+                <Input
+                  {...field}
+                  disabled={true}
+                  placeholder="Email"
+                  type="email"
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button
+          type="submit"
+          className="self-start hover:bg-[#2F006B] hover:text-white "
+        >
+          {isLoading ? (
+            <>
+              <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+              Saving
+            </>
+          ) : (
+            'Save User Settings'
+          )}
+        </Button>
+      </form>
+    </Form>
   )
 }
 
