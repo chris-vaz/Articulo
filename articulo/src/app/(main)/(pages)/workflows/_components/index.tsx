25d5c151eccabd2d7a83976912a06a6f386e6867
chris-vaz <chrisvas2001@gmail.com>
2026-02-01 15:51:03 +0300

    fix: workflow retrieval

--- articulo/src/app/(main)/(pages)/workflows/_components/index.tsx
+++ articulo/src/app/(main)/(pages)/workflows/_components/index.tsx
@@ -6,7 +6,7 @@ import { onGetWorkflows } from '../_actions/workflow-connections'
 type Props = {}
 
 const Workflows = async (props: Props) => {
-    const workflows = await onGetWorkflows()
+  const workflows = await onGetWorkflows()
   return (
     <div className="relative flex flex-col gap-4">
       <section className="flex flex-col m-2">
