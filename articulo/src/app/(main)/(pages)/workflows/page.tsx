dbb80434642ec593c7214eadca861ca33274fd60
chris-vaz <chrisvas2001@gmail.com>
2026-02-01 14:42:59 +0300

    ui: Enhanced 'add workflow' screen on workflows

--- articulo/src/app/(main)/(pages)/workflows/page.tsx
+++ articulo/src/app/(main)/(pages)/workflows/page.tsx
@@ -1,25 +1,17 @@
 import React from 'react'
 import WorkflowButton from './_components/workflow-button'
 import Workflows from './_components'
-import EditorCanvas from './editor/[editorId]/_components/editor-canvas'
-import { ConnectionsProvider } from '@/providers/connections-provider'
-import EditorProvider from '@/providers/editor-provider'
 
 type Props = {}
 
 const Page = (props: Props) => {
   return (
-    <div className="flex flex-col relative h-full">
-      <EditorProvider>
-        <ConnectionsProvider>
-          <EditorCanvas />
-        </ConnectionsProvider>
-      </EditorProvider>
-      {/* <h1 className="text-4xl sticky top-0 z-[10] p-6 bg-background/50 backdrop-blur-lg flex items-center border-b justify-between">
+    <div className="flex flex-col relative">
+      <h1 className="text-4xl sticky top-0 z-[10] p-6 bg-background/50 backdrop-blur-lg flex items-center border-b justify-between">
         Workflows
         <WorkflowButton />
       </h1>
-      <Workflows /> */}
+      <Workflows />
     </div>
   )
 }
